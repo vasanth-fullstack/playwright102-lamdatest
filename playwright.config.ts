23806d4e44886902a29368d340b8d0cc7c301be6
vasanth-fullstack <debumode.dev@gmail.com>
2025-07-17 03:27:16 +0530

    Updated package and added windows hyperexecute

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,31 +19,31 @@ const config: PlaywrightTestConfig = {
     //     viewport: { width: 1920, height: 1080 },
     //   },
     // },
-    // {
-    //   name: "chrome:latest:Windows 11@lambdatest",
-    //   use: {
-    //     viewport: { width: 1280, height: 720 },
-    //   },
-    // },
-    // {
-    //   name: "MicrosoftEdge:latest:MacOS Ventura@lambdatest",
-    //   use: {
-    //     ...devices["iPhone 12 Pro Max"],
-    //   },
-    // },
-    // {
-    //   name: "pw-firefox:latest:Windows 11@lambdatest",
-    //   use: {
-    //     viewport: { width: 1280, height: 720 },
-    //   },
-    // },
+    {
+      name: "chrome:latest:Windows 11@lambdatest",
+      use: {
+        viewport: { width: 1280, height: 720 },
+      },
+    },
+    {
+      name: "MicrosoftEdge:latest:MacOS Ventura@lambdatest",
+      use: {
+        ...devices["iPhone 12 Pro Max"],
+      },
+    },
+    {
+      name: "pw-firefox:latest:Windows 11@lambdatest",
+      use: {
+        viewport: { width: 1280, height: 720 },
+      },
+    },
     {
       name: "pw-webkit:latest:Windows 10@lambdatest",
       use: {
         viewport: { width: 1920, height: 1080 },
       },
     },
-    // Config for running tests in local
+    // Config for running tests in locally.
     // {
     //   name: "chrome",
     //   use: {
